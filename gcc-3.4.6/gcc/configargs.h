bd94ba592763210d8d478460bd434c1c53db67c7
Guy Madison <darkaegisagain@gmail.com>
2024-10-24 13:02:41 -0700

    added tools

---
+++ gcc-3.4.6/gcc/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --target=i860-sysv4 --prefix=/opt/i860 --disable-multilib --disable-threads --enable-languages=c --disable-shared --without-headers --disable-libssp --disable-libmpx --disable-libatomic : (reconfigured) ./configure --target=i860-stardent-sysv4 --prefix=/opt/i860 --disable-multilib --disable-threads --enable-languages=c --disable-shared --without-headers --disable-libssp --disable-libmpx --disable-libatomic";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
---
+++ gcc-3.4.6/gcc/test.c
@@ -0,0 +1,9 @@
+
+int main(int argc, char **argv)
+{
+	int a;
+
+	a = a + 2;
+
+	return a;
+}
